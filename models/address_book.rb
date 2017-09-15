07fc4afd4070bceca19d2c4e34fd195861044ab9
Sanyan Sharar <raad34@gmail.com>
2017-09-14 22:55:16 -0400

    remove entry

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,17 +7,27 @@ class AddressBook
         @entries = []
     end
 
-def add_entry(name, phone_number, email)
-    # #9
-    index = 0
-    entries.each do |entry|
-    # #10
-      if name < entry.name
-        break
+  def add_entry(name, phone_number, email)
+      # #9
+      index = 0
+      entries.each do |entry|
+      # #10
+        if name < entry.name
+          break
+        end
+        index+= 1
       end
-      index+= 1
+      # #11
+      entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+    correctIndex = 0
+    entries.each_with_index do |entry, index|
+      if name == entry.name
+        correctIndex = index
+      end
     end
-    # #11
-    entries.insert(index, Entry.new(name, phone_number, email))
+    entries.delete_at(correctIndex)
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe AddressBook do
     end
   end
 
-  describe "#add_entry" do 
+describe "#add_entry" do 
     it "adds only one entry to the address book" do
         book = AddressBook.new
         book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -36,4 +36,15 @@ RSpec.describe AddressBook do
         expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
  end
+
+ describe "#remove_entry" do
+    it "removes only one entry from the address book" do
+        book = AddressBook.new
+        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+        book.add_entry('Sanyan Sharar', '809.321.453', 'raad34@gmail.com')
+        book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+        expect(book.entries.size).to eq(1)
+    end
+end
+
 end
